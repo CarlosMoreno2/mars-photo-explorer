2f5b01a22b51d2ffdfeecb02c1967bc775fd18f4
Carlos <cmoreno-yanes@csub.edu>
2024-02-23 17:45:24 -0800

    initial commit

---
+++ src/components/Error.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const StyledError = styled.div`
+  color: #ff6b6b;
+  background-color: #ffebeb;
+  padding: 10px;
+  margin: 20px 0;
+  border-left: 5px solid #ff6b6b;
+  border-radius: 5px;
+`;
+
+export default StyledError;
